3ed524d869644b831c0e36ffc25fcb5f3e9c7106
Kayla <kaylaputrimaharani@gmail.com>
2025-05-21 09:53:33 +0700

    Pertemuan 12

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Coba;
+use App\Http\Controllers\PegawaiController;
+use App\Http\Controllers\BlogController;
 
 /*
 |--------------------------------------------------------------------------
@@ -36,6 +38,7 @@ Route::get('blog', function () {
 
 Route::get('hello',[Coba::class, 'helloworld']);
 
+//Route untuk tugas - ets
 Route::get('pertama', function () {
 	return view('pertama');
 });
@@ -87,3 +90,16 @@ Route::get('frontend', function () {
 Route::get('danantara', function () {
 	return view('danantara');
 });
+
+Route::get('dosen',[Coba::class, 'index']);
+
+Route::get('/pegawai/{nama}', [PegawaiController::class, 'index']);
+
+Route::get('/formulir', [PegawaiController::class, 'formulir']); //halaman isian formulir
+
+Route::post('/formulir/proses', [PegawaiController::class, 'proses']); //action form
+
+// route blog
+Route::get('/blog', [BlogController::class, 'home']);
+Route::get('/blog/tentang', [BlogController::class, 'tentang']);
+Route::get('/blog/kontak', [BlogController::class, 'kontak']);
